243ec86c897a9007c72a78131d12641f4d14f926
Preeti-Singh-Rao <preetirao200116@gmail.com>
2024-05-17 20:37:41 +0530

    add new file

---
+++ dom.js
@@ -0,0 +1,14 @@
+let mainheading= document.getElementById("main-heading");
+mainheading.textContent='Fruit World';
+mainheading.style.color='orange';
+
+
+let headerdiv= document.getElementById("header");
+headerdiv.style.background="green";
+headerdiv.style.borderBottom='3px solid orange';
+
+let basketheading= document.getElementById("basket-heading");
+basketheading.style.color="green";
+
+let thanks=document.getElementById("thanks");
+thanks.innerHTML='<p>Please visit us again</p>';
