d4f28eb97712a5b5a95054a687b7e6eaaf98f7fb
Preeti-Singh-Rao <preetirao200116@gmail.com>
2024-05-18 20:44:44 +0530

    upload second new file

---
+++ elementbyclass.js
@@ -0,0 +1,6 @@
+let fruit=document.getElementsByClassName("fruit");
+fruit[2].style.background="yellow";
+fruit[0].style.fontWeight='bold';
+fruit[1].style.fontWeight="bold";
+fruit[2].style.fontWeight='bold';
+fruit[3].style.fontWeight='bold';
